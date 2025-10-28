874fd3340a903c3b140a663a6f67bc7fc02d2aee
ErZicky <105796952+ErZicky@users.noreply.github.com>
2025-10-28 23:59:00 +0100

    Initial commit

---
+++ extension.js
@@ -0,0 +1,171 @@
+/* extension.js
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
+import Gio from 'gi://Gio'; // for file e directory
+import GLib from 'gi://GLib'; // for async
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+
+const VALID_EXTENSIONS = ['.jpg', '.jpeg', '.png', '.webp'];
+
+// gsetting schema for bg
+const BACKGROUND_SCHEMA = 'org.gnome.desktop.background';
+
+const PICTURE_URI_KEY = 'picture-uri';
+
+const PICTURE_URI_DARK_KEY = 'picture-uri-dark';
+
+const WALLPAPERS_DIRECTORY_KEY = 'wallpapers-directory';
+
+// Define a simple localization stub to prevent the '_' is not defined error
+function _(str) {
+    return str;
+}
+
+export default class PlainExampleExtension extends Extension {
+    enable() {
+         this._settings = this.getSettings();
+
+         this._backgroundSettings = new Gio.Settings({ schema: BACKGROUND_SCHEMA });
+
+
+         // wait a few seconds to be sure desktop loaded
+        this._startupId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 3, () => {
+            this._setRandomWallpaper();
+            return GLib.SOURCE_REMOVE;
+        });
+
+        //change wallpaper on login
+         this._unlockSignal = Main.sessionMode.connect('updated', () => {
+        if (!Main.sessionMode.isLocked)
+            this._setRandomWallpaper();
+    });
+         
+    }
+
+    disable() {
+
+        this._settings = null;
+        this._backgroundSettings = null;
+    }
+
+
+    _isValidImage(filename) {
+        const lowerName = filename.toLowerCase();
+        return VALID_EXTENSIONS.some(ext => lowerName.endsWith(ext));
+    }
+
+
+    _setRandomWallpaper() {
+       const folderPath = this._settings.get_string(WALLPAPERS_DIRECTORY_KEY);
+      
+      
+        if (!folderPath) {
+           Main.notify(
+                _('Wallpapers directory does not exist!'), 
+                _('Change directory in settings for BootPaper to work.')
+            );
+            return;
+        }
+
+        const folder = Gio.File.new_for_path(folderPath);
+ 
+        // Check if directory exists and is valid
+        if (!folder.query_exists(null)) {
+            log(`BootPaper: directory does not exist: ${folderPath}`);
+            return;
+        }
+
+        if (folder.query_file_type(Gio.FileQueryInfoFlags.NONE, null) !== Gio.FileType.DIRECTORY) {
+            log(`BootPaper: ${folderPath} is not a directory.`);
+            return;
+        }
+
+
+        folder.enumerate_children_async(
+            'standard::name',
+            Gio.FileQueryInfoFlags.NONE,
+            GLib.PRIORITY_DEFAULT,
+            null, // Cancellable
+            (source, res) => {
+                try {
+                    const enumerator = source.enumerate_children_finish(res);
+                    const wallpapers = [];
+                    let info;
+
+                    // Iterate on files.
+                    while ((info = enumerator.next_file(null)) !== null) {
+                        const fileName = info.get_name();
+
+                        // Check if it's a regular file and a valid image type
+                        if (info.get_file_type() === Gio.FileType.REGULAR && this._isValidImage(fileName)) {
+                            const filePath = GLib.build_filenamev([folderPath, fileName]);
+                            wallpapers.push(filePath);
+                        }
+                    }
+                    enumerator.close(null);
+
+                    if (wallpapers.length === 0) {
+                        log(`BootPaper: no images in: ${folderPath}`);
+                        return;
+                    }
+
+                    
+                    // Select a random image path
+                    const randomIndex = Math.floor(Math.random() * wallpapers.length);
+                    const selectedPath = wallpapers[randomIndex];
+                    
+                    
+                    // Convert the file path to a Gio.File URI
+                    const wallpaperUri = Gio.File.new_for_path(selectedPath).get_uri();
+
+                    // Set the wallpaper URI for both light and dark modes
+                    this._backgroundSettings.set_string(PICTURE_URI_KEY, wallpaperUri);
+                    this._backgroundSettings.set_string(PICTURE_URI_DARK_KEY, wallpaperUri); 
+
+                    
+
+                } catch (e) {
+                    logError(e, 'BootPaper: Error during process');
+                }
+            }
+        );
+    }
+
+     openPreferences() {
+        // Find if an extension preferences window is already open
+        const prefsWindow = global.get_window_actors().map(wa => wa.meta_window).find(w => w.wm_class === 'org.gnome.Shell.Extensions');
+
+        if (!prefsWindow) {
+            super.openPreferences();
+            return;
+        }
+
+        // The current prefsWindow belongs to this extension, activate it
+        if (prefsWindow.title === this.metadata.name) {
+            Main.activateWindow(prefsWindow);
+            return;
+        }
+
+        // If another extension's preferences are open, close it and open this extension's preferences
+        prefsWindow.connectObject('unmanaged', () => {
+            super.openPreferences();
+            prefsWindow.disconnectObject(this);
+        }, this);
+        prefsWindow.delete(global.get_current_time());
+    }
+}
---
+++ prefs.js
@@ -0,0 +1,260 @@
+import Gtk from 'gi://Gtk'
+import Gio from 'gi://Gio';
+import Adw from 'gi://Adw';
+import GObject from 'gi://GObject';
+
+
+//useful to not write gettext before each string
+import {ExtensionPreferences, gettext as _} from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
+
+import * as Config from 'resource:///org/gnome/Shell/Extensions/js/misc/config.js';
+
+
+export default class BootPaperPreferences extends ExtensionPreferences {
+
+
+
+
+    fillPreferencesWindow(window) {
+        // get GSettings object
+        const settings = this.getSettings();
+        
+        // new page
+        const page = new Adw.PreferencesPage({  
+            
+            title: _('General'),
+            icon_name: 'org.gnome.Settings-symbolic',
+        });
+        window.add(page);
+
+        // new group for page
+        const group = new Adw.PreferencesGroup({
+            title: _('Wallpaper Settings'),
+            description: _('Configure the source of your random wallpapers.'),
+        });
+        page.add(group);
+
+        //new row for group
+        const row = new Adw.ActionRow({
+            title: _('wallpaper folder'),
+            subtitle: _('Select the local directory for wallpapers.'),
+            
+        });
+        group.add(row);
+
+
+        const folderButton = new Gtk.Button({
+
+        icon_name: 'folder-open-symbolic',
+        tooltip_text: _('Select directory...'),
+        valign: Gtk.Align.CENTER
+
+        });
+
+        row.add_suffix(folderButton);
+        row.activatable_widget = folderButton;
+
+        //button logic
+        folderButton.connect('clicked', () => {
+    const dialog = new Gtk.FileChooserNative({
+        title: _('Select Wallpapers Folder'),
+        action: Gtk.FileChooserAction.SELECT_FOLDER,
+        transient_for: window,
+        modal: true,
+    });
+
+    dialog.connect('response', (self, response) => {
+        if (response === Gtk.ResponseType.ACCEPT) {
+            const file = dialog.get_file();
+            const folderPath = file.get_path();
+            settings.set_string('wallpapers-directory', folderPath);
+            row.subtitle = folderPath;
+        }
+        dialog.destroy();
+    });
+
+    dialog.show();
+});
+
+
+        const initialPath = settings.get_string('wallpapers-directory');
+        if (initialPath) {
+             row.subtitle = initialPath;
+        } else {
+             row.subtitle = _('No folder selected yet.');
+        }
+
+
+
+        //donate page 
+
+         const donPage = new Adw.PreferencesPage({  
+            
+            title: _('Donate'),
+            icon_name: 'emblem-favorite-symbolic',
+        });
+
+        window.add(donPage);
+
+
+        const donGroup = new Adw.PreferencesGroup({
+            title: _('Support BootPaper Developer'),
+            description: _('Thank you for using BootPaper! If your enjoying it and would like to support it, please consider making a donation')
+        });
+
+
+        //new row for group
+        const Donrow = new Adw.ActionRow({
+            title: _('Buy me a coffee on ko-fi') ,
+            subtitle: _(''),
+            
+        });
+        
+       
+        const kofiPicture = new Gtk.Image({
+            file: this.dir.get_child('media').get_child('klogo.png').get_path(), 
+            valign: Gtk.Align.CENTER, 
+            pixel_size: 48, 
+        });
+
+         
+        const kofiButton = new Gtk.Button({
+             icon_name: 'adw-external-link-symbolic', 
+            css_classes: ['pill'], 
+             valign: Gtk.Align.CENTER,
+        }); 
+        
+        kofiButton.connect('clicked', () => {
+            const uri = 'https://ko-fi.com/zetadev'; 
+            
+            try {
+                 
+                 Gio.AppInfo.launch_default_for_uri(uri, null);
+            } catch (e) {
+                 logError(e, 'Something went wrong');
+            }
+        });
+
+        Donrow.add_suffix(kofiButton); 
+        Donrow.add_prefix(kofiPicture); 
+
+        donGroup.add(Donrow); 
+        
+        
+       donPage.add(donGroup);  
+
+        //about page
+        const abtPage = new Adw.PreferencesPage({  
+             
+            title: _('About'),
+            icon_name: 'info-outline-symbolic',
+        });
+
+       window.add(abtPage);
+
+    const headerBox = new Gtk.Box({
+        orientation: Gtk.Orientation.VERTICAL,
+        halign: Gtk.Align.CENTER,
+        spacing: 6,
+        margin_top: 24,
+        margin_bottom: 12,
+    });
+
+    const iconImage = new Gtk.Image({
+        file: this.dir.get_child('media').get_child('ico.jpg').get_path(),
+        pixel_size: 146,
+        halign: Gtk.Align.CENTER,
+    });
+
+    const titleLabel = new Gtk.Label({
+        label: 'BootPaper',
+        css_classes: ['title-1'],
+        halign: Gtk.Align.CENTER,
+    });
+
+    const subtitleLabel = new Gtk.Label({
+        label: _('Simple wallpaper randomizer on boot for GNOME.'),
+        css_classes: ['dim-label'],
+        halign: Gtk.Align.CENTER,
+        wrap: true,
+    });
+
+
+    const versionPill = new Gtk.Button({
+    label: `${this.metadata.version}`,
+    css_classes: ['pill', 'accent', 'heading'],
+    sensitive: true,
+    halign: Gtk.Align.CENTER,
+});
+    versionPill.set_margin_top(6);
+    versionPill.set_margin_bottom(6);
+
+    headerBox.append(iconImage);
+    headerBox.append(titleLabel);
+    headerBox.append(subtitleLabel);
+    headerBox.append(versionPill);
+
+    const headerGroup = new Adw.PreferencesGroup();
+    headerGroup.add(headerBox);
+    abtPage.add(headerGroup);
+
+
+
+    const abtGroup = new Adw.PreferencesGroup();
+    abtPage.add(abtGroup);
+
+
+
+
+    const gnomeRow = new Adw.ActionRow({
+        title: _('GNOME Version'),
+        subtitle: Config.PACKAGE_VERSION.toString(),
+    });
+    abtGroup.add(gnomeRow);
+
+    // author
+    const authorRow = new Adw.ActionRow({
+        title: _('Created by'), 
+        subtitle: 'ZetaDev',
+    });
+    abtGroup.add(authorRow);
+
+
+    const gitRow = new Adw.ActionRow({
+        title: _('GitHub'),
+    });
+    const gitButton = new Gtk.Button({
+        icon_name: 'adw-external-link-symbolic',
+        valign: Gtk.Align.CENTER,
+    });
+    gitButton.connect('clicked', () => {
+        Gio.AppInfo.launch_default_for_uri('https://github.com/ZetaDev/BootPaper', null);
+    });
+
+    gitRow.add_suffix(gitButton);
+    gitRow.activatable_widget = gitButton;
+    abtGroup.add(gitRow);
+
+
+
+     const warrantyLabel = _('This program comes with absolutely no warranty.');
+     const urlLabel = _('See the <a href="%s">%s</a> for details.').format('https://gnu.org/licenses/old-licenses/gpl-2.0.html', _('GNU General Public License, version 2 or later'));
+
+    const gnuSofwareLabel = new Gtk.Label({
+            label: `${_(warrantyLabel)}\n${_(urlLabel)}`,
+            use_markup: true,
+            justify: Gtk.Justification.CENTER,
+    });   
+     gnuSofwareLabel.set_margin_top(12);
+    gnuSofwareLabel.set_margin_bottom(12);
+
+    const footerGroup = new Adw.PreferencesGroup();
+    footerGroup.add(gnuSofwareLabel);
+    abtPage.add(footerGroup);
+
+
+
+    } 
+}
+
+
